03dd726680499ae944e0d22ee592f588224e07de
Tiago Etcheverry <e.o.j.tiago@gmail.com>
2023-06-21 20:46:06 -0300

    modificacion de funcion 'mostrarCarrito' para que sea un ciclo y agregado de prompts al inicio y final de la transaccion

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // Variables globales
 const carrito = [];
 let totalCarrito = 0;
+let nombreUsuario = "";
+let localidadEnvio = "";
+
+// Solicitar nombre de usuario y localidad de envío al inicio
+nombreUsuario = prompt("Hola, cual es tu nombre?");
+localidadEnvio = prompt("A donde enviamos tu compra?");
+
+alert(`Hola, ${nombreUsuario}! Bienvenido a nuestra tienda. Podes empezar a agregar productos al carrito.`);
 
 // Función para agregar un producto al carrito
 function agregarAlCarrito(nombre, imagen, precio) {
@@ -26,7 +34,9 @@ function mostrarCarrito() {
     const carritoBody = document.getElementById('carrito-body');
     carritoBody.innerHTML = '';
 
-    carrito.forEach((producto) => {
+    for (let i = 0; i < carrito.length; i++) {
+        const producto = carrito[i];
+
         const row = document.createElement('tr');
         row.innerHTML = `
             <td><img src="${producto.imagen}" alt="${producto.nombre}"></td>
@@ -34,13 +44,13 @@ function mostrarCarrito() {
             <td class="color__precio">$${producto.precio}</td>
         `;
         carritoBody.appendChild(row);
-    });
+    }
 }
 
 // Función para finalizar la compra
 function finalizarCompra() {
     if (carrito.length > 0) {
-        alert(`Gracias por comprar nuestros productos. El total es: $${totalCarrito}`);
+        alert(`Gracias ${nombreUsuario} por comprar en nuestra tienda. El total es $${totalCarrito} y el envio a ${localidadEnvio} es de $1200`);
     } else {
         alert('Tu carrito está vacío. Agrega un producto para finalizar la compra.');
     }
