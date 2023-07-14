9135f73d5f06fa9f428a252d8cf25dc17f6959e3
Tiago Etcheverry <e.o.j.tiago@gmail.com>
2023-07-14 00:04:49 -0300

    modificaciones para pre-entrega 3

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
 // Variables globales
-const carrito = [];
+let carrito = [];
 let totalCarrito = 0;
 let nombreUsuario = "";
+let opcionLocalidad = "";
 
-// Solicitar nombre de usuario y localidad de envío al inicio
-nombreUsuario = prompt("Hola, cual es tu nombre?");
-
-const envioPorLocalidad = {
-    "Carmen de Patagones": 200,
-    "Viedma": 300,
-    "San Antonio Oeste": 800,
-    "Las Grutas": 800,
-    "Stroeder": 500,
-    "Bahía Blanca": 1000,
-};
-
-const localidades = Object.keys(envioPorLocalidad);
-let opcionLocalidad = parseInt(prompt(
-    "Elige la localidad de envío:\n" +
-    localidades.map((localidad, index) => `${index + 1}. ${localidad}`).join("\n")
-));
-
-let envioLocalidad;
-if (opcionLocalidad >= 1 && opcionLocalidad <= localidades.length) {
-    envioLocalidad = localidades[opcionLocalidad - 1];
-} else {
-    envioLocalidad = "Localidad desconocida";
+// Traer carrito del local (si es que existe)
+const carritoLocalStorage = localStorage.getItem("carrito");
+if (carritoLocalStorage) {
+    carrito = JSON.parse(carritoLocalStorage);
+    mostrarCarrito();
 }
 
-alert(`Hola, ${nombreUsuario}! Bienvenido a nuestra tienda. Podes empezar a agregar productos al carrito.`);
+// Traer data del DOM
+const nombreUsuarioInput = document.getElementById("nombreUsuario");
+const opcionLocalidadSelect = document.getElementById("opcionLocalidad");
+const botonCompra = document.getElementById("boton-compra");
+
+// Cambio de localidad
+opcionLocalidadSelect.addEventListener("change", () => {
+    opcionLocalidad = opcionLocalidadSelect.value;
+});
 
 // Función para agregar productos al carrito
 function agregarAlCarrito(nombre, imagen, precio, cantidad) {
@@ -39,14 +30,18 @@ function agregarAlCarrito(nombre, imagen, precio, cantidad) {
     } else {
         carrito.push({ nombre, imagen, precio, cantidad });
     }
-    // Mostrar el carrito en la interfaz
+
+    // Guardar carrito en el local
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+
+    // Mostrar el carrito
     mostrarCarrito();
 }
 
 // Función para mostrar los productos en el carrito
 function mostrarCarrito() {
     const carritoBody = document.getElementById("carrito-body");
-    const totalCarritoElement = document.getElementById("total-carrito");
+
     carritoBody.innerHTML = "";
 
     let montoTotal = 0;
@@ -58,11 +53,11 @@ function mostrarCarrito() {
 
         const fila = document.createElement("tr");
         fila.innerHTML = `
-            <td><img src="${imagen}" alt="${nombre}" width="50"></td>
-            <td>${nombre}</td>
-            <td class="color__precio">$${precio}</td>
-            <td>${cantidad}</td>
-    `;
+        <td><img src="${imagen}" alt="${nombre}" width="50"></td>
+        <td>${nombre}</td>
+        <td class="color__precio">$${precio}</td>
+        <td>${cantidad}</td>
+        `;
 
         carritoBody.appendChild(fila);
     });
@@ -75,30 +70,42 @@ function mostrarCarrito() {
     `;
 
     carritoBody.appendChild(filaTotal);
-
-    // Mostrar el total del carrito y el costo de envío
-    const costoEnvio = envioPorLocalidad[envioLocalidad];
-    const montoTotalConEnvio = montoTotal + costoEnvio;
-    totalCarritoElement.innerHTML = `
-    <p><strong>Costo de envío a ${envioLocalidad}:</strong> $${costoEnvio}</p>
-    <p><strong>El total con el envio es de: </strong>$${montoTotalConEnvio}</p>
-    `;
 }
 
 // Función para finalizar la compra
 function finalizarCompra() {
-    const costoEnvio = envioPorLocalidad[envioLocalidad];
+    if (!opcionLocalidad) {
+        alert("Por favor, selecciona una localidad de envío.");
+        return;
+    }
+
+    const envioPorLocalidad = {
+        "Carmen de Patagones": 200,
+        "Viedma": 300,
+        "San Antonio Oeste": 800,
+        "Las Grutas": 800,
+        "Stroeder": 500,
+        "Bahía Blanca": 1000,
+    };
+
+    const costoEnvio = envioPorLocalidad[opcionLocalidad];
     const montoTotal = carrito.reduce((total, producto) => total + producto.precio * producto.cantidad, 0);
     const montoTotalConEnvio = montoTotal + costoEnvio;
 
     if (carrito.length > 0) {
         alert(`¡Gracias ${nombreUsuario} por comprar en nuestra tienda!` +
-            `  El total de tu compra es de: $${montoTotal}\n` + 
-            ` y el envío a ${envioLocalidad} es de: $${costoEnvio}\n` + 
-            `Todo seria ${montoTotalConEnvio} `
-            );
-    }
-    else {
+            ` El total de tu compra es de: $${montoTotal}\n` +
+            ` El envío a ${opcionLocalidad} tiene un costo de: $${costoEnvio}\n` +
+            ` El monto total con envío es: $${montoTotalConEnvio}.`
+        );
+    } else {
         alert('Tu carrito está vacío. Agrega un producto para finalizar la compra.');
     }
+
+    // Eliminar el carrito del almacenamiento local
+    localStorage.removeItem("carrito");
+
+    // Reiniciar el carrito y la interfaz
+    carrito = [];
+    mostrarCarrito();
 }
